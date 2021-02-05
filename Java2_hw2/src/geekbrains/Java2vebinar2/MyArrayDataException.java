4b55d33624d18fde441bcff48c3a80baed57503b
WokoDoko <76409496+WokoDoko@users.noreply.github.com>
2021-02-05 18:02:33 +0300

    Add files via upload

---
+++ Java2_hw2/src/geekbrains/Java2vebinar2/MyArrayDataException.java
@@ -0,0 +1,9 @@
+package geekbrains.Java2vebinar2;
+
+public class MyArrayDataException extends NumberFormatException {
+
+    public MyArrayDataException(String onlyNumbersAvailable) {
+        super(onlyNumbersAvailable);
+    }
+
+}
